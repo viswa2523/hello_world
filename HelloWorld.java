9ca12bbce5903ffd16261f49f41f56db74510e2c
viswa2523 <103309852+viswa2523@users.noreply.github.com>
2022-04-11 15:30:19 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,5 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, Happy Holi");
     System.out.println("Hello, Happy Long Weekend");
-    System.out.println("Hello, Happy Long Weekend 1");
     }
 }
