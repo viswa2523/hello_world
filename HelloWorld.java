35e6f82c34eabcb3b3fbcd73883745f16e165678
viswa2523 <viswandhareddy.kalluri@gmail.com>
2022-04-11 08:59:40 +0000

    Modified some data

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, Happy monday");
-    System.out.println("Hello, Happy tuesday");
-    System.out.println("Hello, Happy wedensday");
-    System.out.println("Hello, Happy thursday");
-    System.out.println("Hello, Happy friday");
+    System.out.println("Hello, Happy Holi");
     }
 }
